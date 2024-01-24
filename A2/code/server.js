32e220b325e379ce274f158bfef8231ecca898af
Ini Obong <iobong@asu.edu>
2024-01-24 11:32:11 -0700

    changed https, portmap.sh and server.js

--- A2/code/server.js
+++ A2/code/server.js
@@ -1,3 +1,26 @@
+//from bootstrap
+
+/*var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 1234;
+
+app.get("/", function (req, res) {
+    res.redirect("index.html")
+});
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);*/
+
+//from class repository for Assignment 2
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
@@ -16,4 +39,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(errorHandler());
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
-app.listen(port);
+//app.listen(port);
+
+if (require.main === module) { app.listen(port); }
+else{ module.exports = app; }

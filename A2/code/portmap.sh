d86d39e0f85ff001ffeec7fac61567d52bfacdde
Ini Obong <iobong@asu.edu>
2024-01-26 13:52:17 -0700

    changed portmap for EC2 Hosting

--- A2/code/portmap.sh
+++ A2/code/portmap.sh
@@ -1,4 +1,4 @@
-#sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 1234
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 1234
 
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 8080
-sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to 8443
+#sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 8080
+#sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to 8443
